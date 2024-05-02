d7216b342f4d5699a4a3b2c29fd156ba90594bd1
Adriandam1 <aabeijoncarbajo@danielcastelao.org>
2024-05-02 19:42:06 +0200

    usuario atributos + getters/setters

--- src/main/java/org/adrian/Usuario.java
+++ src/main/java/org/adrian/Usuario.java
@@ -1,4 +1,49 @@
 package org.adrian;
 
 public class Usuario {
+    private String nombre;
+    private String password;
+    private Rol rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String password, Rol rol) {
+        this.nombre = nombre;
+        this.password = password;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", password='" + password + '\'' +
+                ", rol=" + rol +
+                '}';
+    }
 }
