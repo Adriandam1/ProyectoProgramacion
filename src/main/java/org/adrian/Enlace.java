c4daaafb65bfb7eaff0e620fbb44ca62f915fd50
Adriandam1 <aabeijoncarbajo@danielcastelao.org>
2024-05-02 19:44:05 +0200

    enlace, contructos + getters/setters

--- src/main/java/org/adrian/Enlace.java
+++ src/main/java/org/adrian/Enlace.java
@@ -1,4 +1,38 @@
 package org.adrian;
 
 public class Enlace {
+    private String nombreEnlace;
+    private String url;
+
+    public Enlace() {
+    }
+
+    public Enlace(String nombreEnlace, String url) {
+        this.nombreEnlace = nombreEnlace;
+        this.url = url;
+    }
+
+    public String getNombreEnlace() {
+        return nombreEnlace;
+    }
+
+    public void setNombreEnlace(String nombreEnlace) {
+        this.nombreEnlace = nombreEnlace;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Enlace{" +
+                "nombreEnlace='" + nombreEnlace + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
 }
