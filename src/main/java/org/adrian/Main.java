7c60e99b947d426e348f34374088893f37d1c0ca
Adriandam1 <aabeijoncarbajo@danielcastelao.org>
2024-05-05 11:49:26 +0200

    Seguimiento_01_23

--- src/main/java/org/adrian/Main.java
+++ src/main/java/org/adrian/Main.java
@@ -4,7 +4,7 @@ package org.adrian;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    //confirmo solucion git
+        System.out.println("Hola mundo!");
 
     }
 }
