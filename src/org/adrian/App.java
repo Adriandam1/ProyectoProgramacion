5584e8d2ddee9ee1192f2fb2f27ca5f2fd9c1739
Adriandam1 <aabeijoncarbajo@danielcastelao.org>
2024-06-01 10:08:18 +0200

    Implementado filereader por el punto Manejo de Ficheros

--- src/org/adrian/App.java
+++ src/org/adrian/App.java
@@ -7,6 +7,9 @@ import org.adrian.view.AppFrame;
 import org.adrian.view.LoginPanel;
 
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
@@ -16,10 +19,7 @@ public class App {
                     DatabaseConnection.createTableIfNotExists(); // Crear la tabla si no existe
 
                     GestorUsuarios gestorUsuarios = new GestorUsuarios();
-                    gestorUsuarios.agregarUsuario(new Usuario("lulu", "123", "admin"));
-                    gestorUsuarios.agregarUsuario(new Usuario("admin", "password", "admin"));
-                    gestorUsuarios.agregarUsuario(new Usuario("user1", "pass1", "user"));
-                    gestorUsuarios.agregarUsuario(new Usuario("user2", "pass2", "user"));
+                    cargarUsuariosDesdeArchivo("usuarios.txt", gestorUsuarios);
 
                     AppFrame frame = new AppFrame();
                     frame.setContentPane(new LoginPanel(frame, gestorUsuarios));
@@ -29,5 +29,22 @@ public class App {
                 }
             }
         });
+    }
+
+    private static void cargarUsuariosDesdeArchivo(String nombreArchivo, GestorUsuarios gestorUsuarios) {
+        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(",");
+                if (partes.length == 3) {
+                    String nombreUsuario = partes[0];
+                    String contraseña = partes[1];
+                    String tipoUsuario = partes[2];
+                    gestorUsuarios.agregarUsuario(new Usuario(nombreUsuario, contraseña, tipoUsuario));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
